2d0d4daaeaf0ec67bf82b64e83f6629d7e86809c
d4wns-l1ght <neovx.23@proton.me>
2025-05-10 02:53:59 +1200

    Update function signatures of site::* fns so they can get the state
    Shoutout sofa for stopping me from using some horrifically ugly solution
    (never trust the docs)

--- src/site/leave.rs
+++ src/site/leave.rs
@@ -1,6 +1,13 @@
+use std::sync::Arc;
+
+use axum::extract::State;
+use tokio::sync::RwLock;
+
+use crate::ring::RingState;
+
 pub async fn get() -> &'static str {
     "You want to leave, but the form means nothing to you...."
 }
-pub async fn post() -> &'static str {
+pub async fn post(state: State<Arc<RwLock<RingState>>>) -> &'static str {
     "You are trying to leave through the form."
 }
