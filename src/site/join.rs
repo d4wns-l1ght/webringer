505de3693e32bfc631cffb33b419ed3f896acce9
d4wns-l1ght <neovx.23@proton.me>
2025-05-10 14:18:06 +1200

    Add (basic) functionality to join::post and leave::post
    The error messages are a bit scuffed because its just the raw SQL error
    but whatever

--- src/site/join.rs
+++ src/site/join.rs
@@ -1,6 +1,10 @@
 use std::sync::Arc;
 
-use axum::extract::State;
+use axum::{
+    extract::{Form, State},
+    response::Html,
+};
+use serde::Deserialize;
 use tokio::sync::RwLock;
 
 use crate::ring::RingState;
@@ -8,6 +12,29 @@ use crate::ring::RingState;
 pub async fn get() -> &'static str {
     "You want to join the webring, and you didn't click the form."
 }
-pub async fn post(state: State<Arc<RwLock<RingState>>>) -> &'static str {
-    "You want to join and you clicked the form!"
+
+#[derive(Debug, Deserialize)]
+pub struct JoinForm {
+    url: String,
+    email: String,
+}
+
+pub async fn post(
+    State(state): State<Arc<RwLock<RingState>>>,
+    Form(data): Form<JoinForm>,
+) -> Html<String> {
+    let state = state.write().await;
+    match sqlx::query!(
+        "INSERT INTO sites (root_url, email) values (?, ?)",
+        data.url,
+        data.email
+    )
+    .execute(&state.database)
+    .await
+    {
+        Ok(_query_outcome) => {
+            Html("You want to join and you clicked the form! An admin will be in contact with you soon to verify your site.".to_owned())
+        }
+        Err(e) => Html(format!("There was an error: {}", e)),
+    }
 }
--- src/site/leave.rs
+++ src/site/leave.rs
@@ -1,6 +1,10 @@
 use std::sync::Arc;
 
-use axum::extract::State;
+use axum::{
+    extract::{Form, State},
+    response::Html,
+};
+use serde::Deserialize;
 use tokio::sync::RwLock;
 
 use crate::ring::RingState;
@@ -8,6 +12,22 @@ use crate::ring::RingState;
 pub async fn get() -> &'static str {
     "You want to leave, but the form means nothing to you...."
 }
-pub async fn post(state: State<Arc<RwLock<RingState>>>) -> &'static str {
-    "You are trying to leave through the form."
+
+#[derive(Debug, Deserialize)]
+pub struct LeaveForm {
+    url: String,
+}
+
+pub async fn post(
+    State(state): State<Arc<RwLock<RingState>>>,
+    Form(data): Form<LeaveForm>,
+) -> Html<String> {
+    let state = state.write().await;
+    match sqlx::query!("DELETE FROM sites WHERE root_url = ?", data.url,)
+        .execute(&state.database)
+        .await
+    {
+        Ok(_query_outcome) => Html("Your site has been removed from the webring!".to_owned()),
+        Err(e) => Html(format!("There was an error: {}", e)),
+    }
 }
