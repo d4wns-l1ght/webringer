bffe41204bc836d21e4ae356c09dab2049e6aa94
d4wns-l1ght <neovx.23@proton.me>
2025-05-11 23:13:14 +1200

    Update /list and /random erroring to be more robust
    Now they fail gracefully on a lack of sites, and return an internal
    server error code if there's an actual problem with the server/database

--- src/site/ring.rs
+++ src/site/ring.rs
@@ -7,7 +7,7 @@ use axum::{
 };
 use serde::Deserialize;
 use tokio::sync::RwLock;
-use tracing::{debug, info, instrument, warn};
+use tracing::{debug, error, info, instrument, warn};
 
 use crate::ring::RingState;
 
@@ -25,10 +25,14 @@ pub async fn next(
     let state = state.read().await;
     match state.get_next(&params.current).await {
         Ok(url) => Redirect::to(&url),
-        Err(e) => {
-            warn!("Error getting next site: {e}");
-            Redirect::to(&params.current)
-        }
+        Err(e) => match e.downcast_ref::<sqlx::Error>() {
+            Some(sqlx::Error::RowNotFound) => Redirect::to("."),
+            _ => {
+                error!("Error getting next site: {e}");
+                info!("Redirecting user to {0}", params.current);
+                Redirect::to(&params.current)
+            }
+        },
     }
 }
 
@@ -41,50 +45,66 @@ pub async fn prev(
     let state = state.read().await;
     match state.get_prev(&params.current).await {
         Ok(url) => Redirect::to(&url),
-        Err(e) => {
-            warn!("Error getting next site: {e}");
-            Redirect::to(&params.current)
-        }
+        Err(e) => match e.downcast_ref::<sqlx::Error>() {
+            Some(sqlx::Error::RowNotFound) => Redirect::to("."),
+            _ => {
+                error!("Error getting next site: {e}");
+                info!("Redirecting user to {0}", params.current);
+                Redirect::to(&params.current)
+            }
+        },
     }
 }
 
 #[instrument]
-pub async fn random(state: State<Arc<RwLock<RingState>>>) -> impl IntoResponse {
+pub async fn random(State(state): State<Arc<RwLock<RingState>>>) -> impl IntoResponse {
     debug!("Locking state for read");
     let state = state.read().await;
-    let site_url = match state.get_random_site().await {
-        Ok(url) => url,
-        Err(e) => {
-            let default_url = "Webring url".to_owned();
-            warn!(
-                "Random site error: {} Defaulting to home url {}",
-                e, &default_url
-            );
-            default_url
+    match state.get_random_site().await {
+        Ok(url) => {
+            info!("Redirecting user to {}", &url);
+            Redirect::to(&url).into_response()
         }
-    };
-    info!("Redirecting user to {}", &site_url);
-    Redirect::to(&site_url)
+        Err(e) => match e.downcast_ref::<sqlx::Error>() {
+            Some(sqlx::Error::RowNotFound) => {
+                let default_url = "Webring url".to_owned();
+                warn!(
+                    "No webring sites available. Defaulting to home url {}",
+                    &default_url
+                );
+                Redirect::to(&default_url).into_response()
+            }
+            _ => {
+                error!("Error when getting random site: {e}");
+                http::StatusCode::INTERNAL_SERVER_ERROR.into_response()
+            }
+        },
+    }
 }
 
 #[instrument]
-pub async fn list(state: State<Arc<RwLock<RingState>>>) -> Html<String> {
+pub async fn list(State(state): State<Arc<RwLock<RingState>>>) -> impl IntoResponse {
     let mut output: String = "<p>Webring sites:</p><ul>".to_owned();
     for url in {
         debug!("Locking state for read");
         let state = state.read().await;
         match state.get_list().await {
             Ok(urls) => urls,
-            Err(e) => {
-                warn!("There was an error getting the list of sites: {}", e);
-                return Html(
-                "There was an error getting the list of sites, please try again in a few minutes".to_owned(),
-            );
-            }
+            Err(e) => match e.downcast_ref::<sqlx::Error>() {
+                Some(sqlx::Error::RowNotFound) => {
+                    info!("No rows found for list");
+                    vec![]
+                }
+                _ => {
+                    error!("Error when getting the list of sites: {}", e);
+                    return http::StatusCode::INTERNAL_SERVER_ERROR.into_response()
+                }
+            },
         }
     } {
         output = output + &format!("<li><a href=\"{url}\">{url}</a></li>");
     }
     output += "</ul>";
-    Html(output)
+    debug!("Sending html: {output}");
+    Html(output).into_response()
 }
