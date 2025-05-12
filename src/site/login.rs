37c6640032af1045a19b1c615ee662fe6429341f
d4wns-l1ght <neovx.23@proton.me>
2025-05-12 14:20:23 +1200

    Add basic login::get functionality

--- src/site/login.rs
+++ src/site/login.rs
@@ -1,7 +1,53 @@
-pub async fn get() -> &'static str {
-    "Here's the login page!"
+use axum::{
+    Form,
+    extract::Query,
+    response::{IntoResponse, Redirect},
+};
+use serde::Deserialize;
+use tracing::error;
+
+use crate::ring::auth::{AuthSession, Credentials};
+
+#[derive(Debug, Deserialize)]
+pub struct NextUrl {
+    next: Option<String>,
 }
 
-pub async fn post() -> &'static str {
-    "This is where I'd put by authentication... if I had any"
+pub async fn post(
+    mut auth_session: AuthSession,
+    Form(creds): Form<Credentials>,
+) -> impl IntoResponse {
+    let admin = match auth_session.authenticate(creds.clone()).await {
+        Ok(Some(admin)) => admin,
+        Ok(None) => {
+            // TODO: User response that they didn't log in
+            let mut login_url = "/login".to_owned();
+            if let Some(next) = creds.next {
+                login_url = format!("{}?next={}", login_url, next)
+            };
+
+            return Redirect::to(&login_url);
+        }
+        Err(e) => {
+            // TODO: User response to tell about error
+            error!("Error when authenticating admin: {}", e);
+            return Redirect::to(".");
+        }
+    };
+
+    if let Err(e) = auth_session.login(&admin).await {
+        error!("Error when logging in admin: {}", e);
+        return Redirect::to(".");
+    }
+
+    if let Some(ref next) = creds.next {
+        Redirect::to(next)
+    } else {
+        Redirect::to("/")
+    }
+}
+
+pub async fn get(Query(NextUrl { next: _next }): Query<NextUrl>) -> &'static str {
+    // TODO: Add templates
+    "This is where you log in"
 }
