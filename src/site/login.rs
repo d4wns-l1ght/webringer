df87423ae5def4376053069ee8a308edb35b35e9
d4wns-l1ght <neovx.23@proton.me>
2025-05-12 15:50:27 +1200

    Add very basic admin login page

--- src/site/login.rs
+++ src/site/login.rs
@@ -1,13 +1,23 @@
+use askama::Template;
 use axum::{
     Form,
     extract::Query,
-    response::{IntoResponse, Redirect},
+    http::StatusCode,
+    response::{Html, IntoResponse, Redirect},
 };
 use serde::Deserialize;
-use tracing::error;
+use tracing::{debug, error};
 
 use crate::ring::auth::{AuthSession, Credentials};
 
+#[derive(Template)]
+#[template(path = "login.html")]
+pub struct LoginTemplate {
+    next: Option<String>,
+}
+
+// This allows us to extract the "next" field from the query string. We use this
+// to redirect after log in.
 #[derive(Debug, Deserialize)]
 pub struct NextUrl {
     next: Option<String>,
@@ -18,9 +28,13 @@ pub async fn post(
     Form(creds): Form<Credentials>,
 ) -> impl IntoResponse {
     let admin = match auth_session.authenticate(creds.clone()).await {
-        Ok(Some(admin)) => admin,
+        Ok(Some(admin)) => {
+            debug!("Authenticated admin {:?}", &admin);
+            admin
+        }
         Ok(None) => {
             // TODO: User response that they didn't log in
+            debug!("Authentication failed with credentials {:?}", &creds);
             let mut login_url = "/login".to_owned();
             if let Some(next) = creds.next {
                 login_url = format!("{}?next={}", login_url, next)
@@ -41,13 +55,24 @@ pub async fn post(
     }
 
     if let Some(ref next) = creds.next {
+        debug!("Login successful, redirecting to {}", &next);
         Redirect::to(next)
     } else {
-        Redirect::to("/")
+        debug!("Login successful, redirecting to /");
+        Redirect::to("/admin")
     }
 }
 
-pub async fn get(Query(NextUrl { next: _next }): Query<NextUrl>) -> &'static str {
-    // TODO: Add templates
-    "This is where you log in"
+pub async fn get(Query(NextUrl { next }): Query<NextUrl>) -> impl IntoResponse {
+    let t = LoginTemplate { next };
+    match t.render() {
+        Ok(s) => {
+            debug!("Successfully rendered login html");
+            Html(s).into_response()
+        }
+        Err(e) => {
+            error!("Error when rendering login html: {}", e);
+            StatusCode::INTERNAL_SERVER_ERROR.into_response()
+        }
+    }
 }
