98e0a058d8c4a0950cb7f4cdd5e5fc17dddb446c
d4wns-l1ght <neovx.23@proton.me>
2025-05-15 11:31:33 +1200

    Add basic landing page for admin user

--- src/site/admin/mod.rs
+++ src/site/admin/mod.rs
@@ -1,8 +1,12 @@
+use askama::Template;
 use axum::{
     Router,
+    http::StatusCode,
+    response::{Html, IntoResponse},
     routing::{get, post},
 };
 use axum_login::login_required;
+use tracing::{debug, error};
 
 use crate::ring::RingState;
 
@@ -23,8 +27,21 @@ pub fn router(state: RingState) -> Router {
         .route_layer(login_required!(RingState, login_url = "/login"))
 }
 
-async fn landing_page() -> &'static str {
-    "TODO! Admin view"
+#[derive(Template)]
+#[template(path = "admin/landing_page.html")]
+pub struct AdminLandingPageTemplate {}
+
+async fn landing_page() -> impl IntoResponse {
+    match { AdminLandingPageTemplate {} }.render() {
+        Ok(s) => {
+            debug!("Successfully rendered admin landing page html");
+            Html(s).into_response()
+        }
+        Err(e) => {
+            error!("Error when rendering login html: {}", e);
+            StatusCode::INTERNAL_SERVER_ERROR.into_response()
+        }
+    }
 }
 
 async fn view() -> &'static str {
