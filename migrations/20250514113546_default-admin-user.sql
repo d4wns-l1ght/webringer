55e12d5f33262bd9b74b0b1b93e416f793189e75
d4wns-l1ght <neovx.23@proton.me>
2025-05-15 00:29:33 +1200

    Add sqlx migration to add default admin user with password admin
    The ideal workflow is to login as this account, add a custom admin
    account (with a good password, and then delete the account)

---
+++ migrations/20250514113546_default-admin-user.sql
@@ -0,0 +1,6 @@
+INSERT INTO admins (username, email, password_phc) VALUES (
+    'admin',
+    'admin@webringer.com',
+    '$argon2id$v=19$m=19456,t=2,'
+    || 'p=1$DT+FkPZlCUjEjpAcRhLbsQ$H9PxZNMPYrG0VRHZ2yHPkoP02aO1vSdagwV2pWGAFJc'
+);
