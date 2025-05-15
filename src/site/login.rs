e75d82a9450225df2b308c7ea878aac42d481764
d4wns-l1ght <neovx.23@proton.me>
2025-05-16 01:50:38 +1200

    Add TODO for redirecting when logging in

--- src/site/login.rs
+++ src/site/login.rs
@@ -58,6 +58,7 @@ pub async fn post(
         return Redirect::to(".");
     }
 
+    // TODO: This isn't working
     if let Some(ref next) = creds.next {
         debug!("Login successful, redirecting to {}", &next);
         Redirect::to(next)
