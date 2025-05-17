2787a3e5af48370ac0c9209d6c0d5f3ea7bf620b
d4wns-l1ght <neovx.23@proton.me>
2025-05-17 23:47:20 +1200

    Switch to using forms instead of queries for approving/denying a site
    This is to make it simpler & cleaner to add a reason for denying a site

--- src/site/admin/approve.rs
+++ src/site/admin/approve.rs
@@ -1,5 +1,6 @@
 use axum::{
-    extract::{Query, State},
+    Form,
+    extract::State,
     http::StatusCode,
     response::{IntoResponse, Redirect},
 };
@@ -11,7 +12,7 @@ use tracing::error;
 use crate::ring::{RingState, auth::AuthSession};
 
 #[derive(Debug, Deserialize)]
-pub struct ApproveSiteQuery {
+pub struct ApproveSiteForm {
     url: String,
 }
 
@@ -19,16 +20,16 @@ pub(super) async fn post(
     messages: Messages,
     auth_session: AuthSession,
     State(state): State<RingState>,
-    Query(query): Query<ApproveSiteQuery>,
+    Form(form): Form<ApproveSiteForm>,
 ) -> impl IntoResponse {
     let admin = match auth_session.user {
         Some(admin) => admin,
         None => return StatusCode::UNAUTHORIZED.into_response(),
     };
-    if let Err(e) = state.approve_site(&query.url, admin.id()).await {
+    if let Err(e) = state.approve_site(&form.url, admin.id()).await {
         error!("Error when trying to approve site: {e}");
         return StatusCode::INTERNAL_SERVER_ERROR.into_response();
     }
-    messages.info(format!("Site {} approved", query.url));
+    messages.info(format!("Site {} approved", form.url));
     Redirect::to("/admin/view").into_response()
 }
